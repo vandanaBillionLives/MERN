5ef6fe7d3514136427a90c42ccaa43ca16aab6e5
Vandana <vandana@billionlives.in>
2022-05-25 12:11:51 +0530

    React change

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ export default class App extends React.Component {
     render() {
 
         return (
-            <div style={{
-               c
-            }}>
+            <div style={c}>
                 Hello world
        
             </div>
